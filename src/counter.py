d0069d9e49e21a843e9c614aaba78a53f18081ec
cpilande <120698868+cpilande@users.noreply.github.com>
2023-09-30 15:32:40 -0700

    Update counter.py

--- src/counter.py
+++ src/counter.py
@@ -1,39 +1,39 @@
 from flask import Flask
-from src import status 
+from src import status
 
 app = Flask(__name__)
 
 COUNTERS = {}
+
 
 # We will use the app decorator and create a route called slash counters.
 # specify the variable in route <name>
 # let Flask know that the only methods that is allowed to called
 # on this function is "POST".
 @app.route('/counters/<name>', methods=['POST'])
-
 def create_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
 
     if name in COUNTERS:
-        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
-    
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+
     COUNTERS[name] = 0
-    print(COUNTERS) 
+    print(COUNTERS)
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
-@app.route('/counters/<name>', methods=['PUT'])
 
+@app.route('/counters/<name>', methods=['PUT'])
 def update_counter(name):
     COUNTERS[name] = COUNTERS[name] + 1
     return {name: COUNTERS[name]}, status.HTTP_200_OK
 
-@app.route('/counters/<name>', methods=['GET'])
 
+@app.route('/counters/<name>', methods=['GET'])
 def read_counter(name):
     if name in COUNTERS.keys():
         print(COUNTERS[name])
         return {name: COUNTERS[name]}, status.HTTP_200_OK
-    
+
     return name, status.HTTP_404_NOT_FOUND
